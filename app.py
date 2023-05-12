4a6b548a861859406afdcd47c6907271eb7ac759
Nare Chitturi <nare_chitturi@yahoo.com>
2023-05-12 08:37:32 -0400

    v7

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly_express as px
 
-df = pd.read_csv('C:/Users/Dr_Na/Git_Repositories/Practicum_Project/vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv')
 
 st.header('Price by Type of vehicle')
 
