13a925d103100905ecf56f8504ea0ce94e724d38
Nare Chitturi <nare_chitturi@yahoo.com>
2023-05-12 08:35:46 -0400

    v6

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly_express as px
 
 df = pd.read_csv('C:/Users/Dr_Na/Git_Repositories/Practicum_Project/vehicles_us.csv')
 
