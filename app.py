d7f8b9cac172e45c897a533dbda4a5b86e73b63d
Nare Chitturi <nare_chitturi@yahoo.com>
2023-05-12 08:30:43 -0400

    v5

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+df = pd.read_csv('C:/Users/Dr_Na/Git_Repositories/Practicum_Project/vehicles_us.csv')
+
 st.header('Price by Type of vehicle')
 
 fig = px.histogram(df, x = ['price'], color = 'type')
